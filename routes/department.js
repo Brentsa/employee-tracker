e34dc4a79b70fafe7c4293e7a70a8a7b0f340251
Solidshots <seanbrent5@gmail.com>
2021-05-10 19:42:11 -0400

    removed some excess files

--- routes/department.js
+++
@@ -1,7 +0,0 @@
-// presented with the following options: view all departments, add a department
-
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
