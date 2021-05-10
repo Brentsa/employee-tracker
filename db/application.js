eb35f0772e62ec8eb5fffb303417a035b5920c7f
Solidshots <seanbrent5@gmail.com>
2021-05-10 19:41:41 -0400

    View all tables are functioning

--- db/application.js
+++ db/application.js
@@ -4,9 +4,12 @@ const inquirer = require('inquirer');
 require('dotenv').config()
 
 class Application{
-    constructor(){
+    constructor()
+    {
+        //Set up the db connection when an application is instantiated
         this.db = mysql.createConnection({ host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS, database: 'human_resources' });
 
+        //The main menu question that will first be presented to the user via inquirer
         this.mainMenuQuestion = {
             type: "list",
             name: "selection",
@@ -15,8 +18,10 @@ class Application{
         };
     }
 
+    //execute this function to begin the app and start the interation loop
     runApplication()
     {
+        //run inquirer
         inquirer
         .prompt(this.mainMenuQuestion)
         .then(({selection}) => {
@@ -24,23 +29,19 @@ class Application{
         });
     }
 
+    //Handle the results from the user selection
     displayResults(selection)
     {
+        //Determine what the app should do based on the respose from inquirer
         switch(selection){
             case "View all departments":
-                console.log(1);
-                this.runApplication();
-                break;
+                return this.displayAllDepartments();
     
             case "View all roles":
-                console.log(2);
-                this.runApplication();
-                break;
+                return this.displayAllRoles();
             
             case "View all employees":
-                console.log(3);
-                this.runApplication();
-                break;
+                return this.displayAllEmployees();
     
             case "Add a department":
                 console.log(4);
@@ -63,11 +64,92 @@ class Application{
                 break;
     
             case "Exit":
-                console.log("Not ready.");
+                console.log("\nThanks and take care!");
                 this.db.end();
                 break;
         }
     }
+
+    //WHEN I choose to view all departments
+    //THEN I am presented with a formatted table showing department names and department ids
+    //Returns a promise that displays all departments in a table
+    displayAllDepartments(){
+        const sql = "SELECT * FROM departments";
+                
+        return this.db.promise().query(sql)
+        .then( ([rows]) => {
+
+            //log a line break and then a table with data
+            console.log("");
+            console.table(rows);
+
+            //rerun the main application loop
+            this.runApplication();
+        })
+        .catch( err => {
+            //catch the error, display the error message and restart the loop
+            console.log(`\nError: ${err.message}\n`);
+            this.runApplication();
+        });
+    }
+
+    //WHEN I choose to view all roles
+    //THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+    //Returns a promise that displays all roles in a table
+    displayAllRoles(){
+        const sql = "SELECT * FROM roles";
+                
+        return this.db.promise().query(sql)
+        .then( ([rows]) => {
+
+            //log a line break and then a table with data
+            console.log("");
+            console.table(rows);
+
+            //rerun the main application loop
+            this.runApplication();
+        })
+        .catch( err => {
+            //catch the error, display the error message and restart the loop
+            console.log(`\nError: ${err.message}\n`);
+            this.runApplication();
+        });
+    }
+
+    //WHEN I choose to view all employees
+    //THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    //Returns a promise that displays all employees in a table
+    displayAllEmployees(){
+        const sql = "SELECT * FROM employees";
+
+        return this.db.promise().query(sql)
+        .then( ([rows]) => {
+
+            //log a line break and then a table with data
+            console.log("");
+            console.table(rows);
+
+            //rerun the main application loop
+            this.runApplication();
+        })
+        .catch( err => {
+            //catch the error, display the error message and restart the loop
+            console.log(`\nError: ${err.message}\n`);
+            this.runApplication();
+        });
+    }
+
+    //WHEN I choose to add a department
+    //THEN I am prompted to enter the name of the department and that department is added to the database
+
+    //WHEN I choose to add an employee
+    //THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
+
+    //WHEN I choose to update an employee role
+    //THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+
+    // WHEN I choose to add a role
+    // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 }
 
 module.exports = Application;
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-
 const Application = require('./db/application');
 
+//Create an instance of an application
 const instance = new Application;
 
+//Run the application
 instance.runApplication();
